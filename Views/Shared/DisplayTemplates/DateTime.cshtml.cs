a3ea0d4241fb9ee4c48e5cdabe40d94457a7ee31
SergioTrinquet <serge.trinquet@gmail.com>
2026-01-13 15:16:55 +0100

    chore: Ajout DisplayTemplates pour afficher tiret qd pas data s views

---
+++ Views/Shared/DisplayTemplates/DateTime.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoListWebApp.Views.Shared.DisplayTemplates
+{
+    public class DateTimeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Shared/DisplayTemplates/String.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoListWebApp.Views.Shared.DisplayTemplates
+{
+    public class StringModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
